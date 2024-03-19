930412ee135de09107c69e71374b3154e3036ecc
Omar Contreras <108770789+0marContreras@users.noreply.github.com>
2024-03-19 14:01:20 -0600

    Register working

--- lib/general.dart
+++ lib/general.dart
@@ -1,30 +1,54 @@
 import 'package:flutter/material.dart';
 
-class GeneralPage extends StatelessWidget {
+void main() {
+  runApp(MaterialApp(
+    home: GeneralPage(),
+  ));
+}
+
+class GeneralPage extends StatefulWidget {
+  @override
+  _GeneralPageState createState() => _GeneralPageState();
+}
+
+class _GeneralPageState extends State<GeneralPage> {
+  int _selectedChatIndex = -1;
+
+  void _selectChat(int index) {
+    setState(() {
+      _selectedChatIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(219, 35, 2, 93),
       appBar: AppBar(
         title: Text('General'),
       ),
       body: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Columna para la lista de chats
+          // Column for the list of chats
           Container(
-            width: 200, // Ancho de la lista de chats
-            color: Colors.grey[200],
+            width: 200, // Width of the chat list
+            color: Color.fromARGB(219, 35, 2, 93), // Background color for chat list
             child: ListView.builder(
-              itemCount: 10, // Número arbitrario de elementos
+              itemCount: 10, // Arbitrary number of elements
               itemBuilder: (context, index) {
-                return ChatItem(index: index); // Cada elemento es un ChatItem interactivo
+                return ChatItem(
+                  index: index,
+                  isSelected: index == _selectedChatIndex,
+                  onSelect: _selectChat,
+                ); // Each element is an interactive ChatItem
               },
             ),
           ),
-          // Columna para el contenido del chat
+          // Column for the chat content
           Expanded(
             child: Container(
-              color: Colors.white,
+              color: Color.fromARGB(255, 90, 14, 161), // Background color for chat content
               child: Center(
                 child: Text('Contenido del Chat'),
               ),
@@ -34,73 +58,125 @@ class GeneralPage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Navega de regreso a la página de inicio de sesión
-          Navigator.pop(context);
+          if (_selectedChatIndex != -1) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChatDetailScreen(
+                  chatIndex: _selectedChatIndex,
+                ),
+              ),
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Select a chat to send a message.'),
+              ),
+            );
+          }
         },
-        child: Icon(Icons.logout),
+        child: Icon(Icons.send),
       ),
     );
   }
 }
 
-class ChatItem extends StatefulWidget {
-  final int index;
+class ChatDetailScreen extends StatefulWidget {
+  final int chatIndex;
 
-  const ChatItem({Key? key, required this.index}) : super(key: key);
+  const ChatDetailScreen({Key? key, required this.chatIndex}) : super(key: key);
 
   @override
-  _ChatItemState createState() => _ChatItemState();
+  _ChatDetailScreenState createState() => _ChatDetailScreenState();
 }
 
-class _ChatItemState extends State<ChatItem> {
-  bool isExpanded = false;
+class _ChatDetailScreenState extends State<ChatDetailScreen> {
+  List<String> _messages = [];
+  TextEditingController _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat ${widget.chatIndex}'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_messages[index]),
+                );
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      hintText: 'Type a message...',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    _sendMessage();
+                  },
+                  icon: Icon(Icons.send),
+                  
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _sendMessage() {
+    String message = _textController.text;
+    if (message.isNotEmpty) {
+      setState(() {
+        _messages.add(message);
+        _textController.clear();
+      });
+    }
+  }
+}
+
+class ChatItem extends StatelessWidget {
+  final int index;
+  final bool isSelected;
+  final Function(int) onSelect;
+
+  const ChatItem({
+    Key? key,
+    required this.index,
+    required this.isSelected,
+    required this.onSelect,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        setState(() {
-          isExpanded = !isExpanded; // Cambia el estado al hacer clic
-        });
+        onSelect(index);
       },
       child: Container(
         padding: EdgeInsets.all(10),
-        color: isExpanded ? Colors.blue[100] : null, // Color de fondo cuando se expande
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Chat ${widget.index}',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            if (isExpanded) // Muestra el contenido del chat solo si está expandido
-              Expanded(
-                child: Stack(
-                  children: [
-                    // Contenido del chat
-                    Container(
-                      color: Colors.white,
-                      child: Center(
-                        child: Text('Contenido del Chat ${widget.index}'),
-                      ),
-                    ),
-                    // Botón para cerrar el chat
-                    Positioned(
-                      top: 8,
-                      right: 8,
-                      child: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            isExpanded = false; // Cerrar el chat al presionar el botón
-                          });
-                        },
-                        icon: Icon(Icons.close),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-          ],
+        color: isSelected ? Color.fromARGB(255, 26, 0, 51) : null, // Color when selected
+        child: Text(
+          'Chat $index',
+          style: TextStyle(
+            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+            color: Colors.white, // Text color
+          ),
         ),
       ),
     );
--- lib/register.dart
+++ lib/register.dart
@@ -1,110 +1,153 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'login.dart';
+
 
 class RegisterPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
-  
+
   final TextEditingController passwordController = TextEditingController();
-  
+
   final TextEditingController userController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF2C2F33),
+      backgroundColor: Color.fromARGB(219, 35, 2, 93),
       appBar: AppBar(
         title: Text('Register'),
       ),
       body: Center(
-        child:Container(
-          width: MediaQuery.of(context).size.width* 0.8,
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.8,
           height: MediaQuery.of(context).size.height * 0.7,
-        padding: EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Color(0xFF40444B),
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.5),
-              spreadRadius: 2,
-              blurRadius: 5,
-              offset: Offset(0,3),
-            ),
-          ],
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(219, 35, 2, 93),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 2,
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Aquí está la imagen
+              Image.asset(
+                'assets/LogoBiscord.jpeg', // Ruta de la imagen en tus activos
+                width: 70, // Ancho deseado de la imagen
+                height: 70, // Alto deseado de la imagen
+                fit: BoxFit.cover, // Ajuste de la imagen dentro del contenedor
+              ),
+              const SizedBox(height: 20), // Espacio adicional
+              Container(
+                width: 400,
+                height: 50,
+                child: TextField(
+                  controller: userController,
+                  style: TextStyle(color: Color.fromARGB(255, 21, 21, 21)),
+                  decoration: InputDecoration(
+                    hintText: 'User name',
+                    hintStyle: TextStyle(
+                        color: const Color.fromARGB(153, 107, 107, 107)),
+                    fillColor: Color.fromARGB(255, 255, 255, 255),
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+
+              
+              TextField(
+                controller: emailController,
+                style: TextStyle(color: const Color.fromARGB(255, 17, 17, 17)),
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  hintStyle: TextStyle(
+                      color: const Color.fromARGB(153, 107, 107, 107)),
+                  fillColor: Color.fromARGB(255, 255, 255, 255),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              TextField(
+                controller: passwordController,
+                style: TextStyle(color: const Color.fromARGB(255, 17, 17, 17)),
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: TextStyle(
+                      color: const Color.fromARGB(153, 107, 107, 107)),
+                  fillColor: Color.fromARGB(255, 255, 255, 255),
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 20,),
+
+              SizedBox(
+                width: 400,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 90, 14, 161)),
+                  onPressed: () async {
+  String username = userController.text;
+  String email = emailController.text;
+  String password = passwordController.text;
+
+  final url = Uri.parse('http://localhost:3000/createUser');
+  final response = await http.post(
+    url,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'username': username,
+      'email': email,
+      'password': password,
+    }),
+  );
+
+  if (response.statusCode == 201) {
+    // Registration successful
+    // Redirect to the login page
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginPage()),
+    );
+  } else {
+    // Registration failed
+    // Handle failure as needed
+    print('Registration failed');
+  }
+},
+                  child: Text('Register',
+                      style: TextStyle(
+                        color: Colors.white,
+                      )),
+                ),
+              ),
+            ],
+          ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: userController,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: 'User Name',
-                hintStyle: TextStyle(color: Colors.white60),
-                fillColor: Color(0xFF40444B),
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-            ),
-            SizedBox(height: 30,),
-            TextField(
-              controller: emailController,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: 'Email',
-                hintStyle: TextStyle(color: Colors.white60),
-                fillColor: Color(0xFF40444B),
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-            ),
-            SizedBox(height: 30),
-            
-            TextField(
-              controller: passwordController,
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: 'Password',
-                hintStyle: TextStyle(color: Colors.white60),
-                fillColor: Color(0xFF40444B),
-                filled: true,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide.none,
-                ),
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 30),
-            
-            
-            SizedBox(
-              width: MediaQuery.of(context).size.width* 0.4,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                onPressed: () {
-                  String user = userController.text;
-                  String email = emailController.text;
-                  
-                  String password = passwordController.text;
-                  
-                  
-                  // Implementa la lógica de registro aquí
-                  print('User: $user, Email: $email, Password: $password,');
-                },
-                child: Text(
-                  'Register',
-                  style: TextStyle(color:  Colors.white,) ),
-              ),
-            ),
-          ],
-        ),
-      ),
       ),
     );
   }
